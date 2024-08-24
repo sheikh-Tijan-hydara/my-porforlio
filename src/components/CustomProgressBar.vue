bb5cf25cc4617f76e27517778d0d60bdc35e6388
sheikh-Tijan-hydara <hydarasheikhtijan30@gmail.com>
2024-08-24 20:00:25 +0000

    .

--- src/components/CustomProgressBar.vue
+++ src/components/CustomProgressBar.vue
@@ -2,40 +2,43 @@
 import ProgressBar from "primevue/progressbar";
 
 const props = defineProps({
-     targetValue: {
-       type: Number,
-       required: true,
-     },
-})
+  targetValue: {
+    type: Number,
+    required: true,
+  },
+});
 
 const value = ref(0);
 
 onMounted(() => {
-     setTimeout(() => {
-       value.value = props.targetValue;
-     }, 1000);
-})
+  setTimeout(() => {
+    value.value = props.targetValue;
+  }, 1000);
+});
 </script>
 
 <template>
-     <ProgressBar
-                :value=value
-                :pt="{
-                  root: {
-                    style: {
-                      height: '14px',
-                      width: '100%',
-                      borderRadius: '8px',
-                    },
-                    class: 'bg-gray-200 overflow-hidden',
-                  },
-                  value: { class: 'bg-green-500 h-full rounded-xl transition-all duration-1000 ease-out' },
-                }"
-                :pt-options="{
-                  mergeSections: false,
-                  mergeProps: false,
-                }"
-              >
-                <span></span>
-              </ProgressBar>
+  <ProgressBar
+    :value="value"
+    :pt="{
+      root: {
+        style: {
+          height: '14px',
+          width: '100%',
+          borderRadius: '8px',
+        },
+        class: 'bg-gray-200 overflow-hidden',
+      },
+      value: {
+        class:
+          'bg-green-500 h-full rounded-xl transition-all duration-1000 ease-out',
+      },
+    }"
+    :pt-options="{
+      mergeSections: false,
+      mergeProps: false,
+    }"
+  >
+    <span></span>
+  </ProgressBar>
 </template>
